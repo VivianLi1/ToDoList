d2c0672d781a0c72b30738ae85d107687445919d
VivianLi1 <vivian2938@gmail.com>
2015-12-03 11:46:22 -0500

    added stuff

--- script.js
+++ script.js
@@ -9,12 +9,31 @@ var removeItem = function(n){
     n.remove();
 };
 
+var highlight = function(n){
+    n.fontcolor = "red";
+}
+
 var buttonCallback = function buttonCallback(e){
     console.log(e);
     console.log(this);
     addItem(document.getElementById("textbox").value);
     document.getElementById("textbox").value = "";
 };
+
+var highlightCallback = function highlightCallback(e){
+    console.log(e);
+    console.log(this);
+    var list = document.getElementById("list");
+    var items = list.children;
+    var counter = 0;
+    if(counter < items.length){
+	highlight(items[counter]);
+	counter++;
+    } else{
+	counter = 0;
+    }
+}
+    
 
 function removeFunction(event) {
     var x = event.target;
@@ -23,6 +42,9 @@ function removeFunction(event) {
 
 var button = document.getElementById('b1');
 b1.addEventListener('click', buttonCallback);
+
+var button2 = document.getElementById('b2');
+b2.addEventListener('click', highlightCallback);
 
 var list = document.getElementById("list");
 var items=list.children;
